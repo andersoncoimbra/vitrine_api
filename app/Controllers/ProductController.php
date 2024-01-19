40e62cc2425fd9b59c2600b7b2df6fcf1c8517e4
Anderson Coimbra <andersonmegax@hotmail.com>
2024-01-18 23:10:26 -0300

    Registro de produtos

---
+++ app/Controllers/ProductController.php
@@ -0,0 +1,41 @@
+<?php
+namespace App\Controllers;
+use App\Models\Product;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class ProductController
+{
+    public function index(Request $request,Response $response, $args)
+    {
+        $products = Product::all();
+
+        $response->getBody()->write(json_encode(['products' => $products]));
+        return $response
+            ->withHeader('Content-Type', 'application/json')
+            ->withStatus(200);
+    }
+
+    public function show(Request $request,Response $response, $args)
+    {
+        $product = Product::find($args['id']);
+
+        $response->getBody()->write(json_encode(['product' => $product]));
+        return $response
+            ->withHeader('Content-Type', 'application/json')
+            ->withStatus(200);
+    }
+
+    public function store(Request $request,Response $response, $args)
+    {
+        $data = json_decode($request->getBody()->getContents(), true);
+
+
+        $product = Product::create($data);
+
+        $response->getBody()->write(json_encode(['product' => $product]));
+        return $response
+            ->withHeader('Content-Type', 'application/json')
+            ->withStatus(201);
+    }
+}
---
+++ app/routes/api.php
@@ -0,0 +1,13 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Vitrine API");
+    return $response;
+});
+
+$app->get('/products', 'App\Controllers\ProductController:index');
+$app->get('/products/{id}', 'App\Controllers\ProductController:show');
+$app->post('/products', 'App\Controllers\ProductController:store');
